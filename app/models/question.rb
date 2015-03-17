ea78517fe342f131fcf18a89158fda2228c77336
tranchinhthuc <thuctc91@gmail.com>
2015-03-17 15:37:44 +0700

    Create question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,6 @@ class Question < ActiveRecord::Base
   has_many :answers
   has_many :options, dependent: :destroy
   belongs_to :course
+
+  accepts_nested_attributes_for :options
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150317031510) do
+ActiveRecord::Schema.define(version: 20150317015242) do
 
   create_table "answers", force: true do |t|
     t.integer  "result_id"
