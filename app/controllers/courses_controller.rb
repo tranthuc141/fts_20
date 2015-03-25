ae7a3737b2132e72ebe761bb741db2dbc2156b49
tranchinhthuc <thuctc91@gmail.com>
2015-03-25 09:40:33 +0700

    Edit something

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,11 @@
 class CoursesController < ApplicationController
   before_action :authenticate_user!
   
-  def show
-    @course = Course.find params[:id]
-  end
-
   def index
     @courses = Course.all.paginate page: params[:page]
+  end
+  
+  def show
+    @course = Course.find params[:id]
   end
 end
